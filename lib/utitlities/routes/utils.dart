580b6c316c8880deb81eef4f8f553fc66cdace5f
Rohitkumar1998 <rohitgupta199920@gmail.com>
2023-09-13 18:50:42 +0530

    add

---
+++ lib/utitlities/routes/utils.dart
@@ -0,0 +1,41 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter/material.dart';
+
+class Utils {
+  static void fieldFocus(
+      BuildContext context, FocusNode current, FocusNode next) {
+    current.unfocus();
+    FocusScope.of(context).requestFocus(next);
+  }
+
+  static toastMessage(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+        toastLength: Toast.LENGTH_SHORT);
+  }
+
+  static void flushBarErrorMessage(String message, BuildContext context) {
+    showFlushbar(
+        context: context,
+        flushbar: Flushbar(
+          message: message,
+          title: "SAD",
+          forwardAnimationCurve: Curves.decelerate,
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          flushbarPosition: FlushbarPosition.TOP,
+          padding: EdgeInsets.all(15),
+          backgroundColor: Colors.yellow,
+          reverseAnimationCurve: Curves.easeInOut,
+          duration: Duration(seconds: 3),
+        )..show(context));
+  }
+
+  static snackBar(String message, BuildContext context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(backgroundColor: Colors.green, content: Text(message)));
+  }
+}
--- lib/view/logScreen.dart
+++ lib/view/logScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm/utitlities/routes/rotuteName.dart';
+import 'package:mvvm/utitlities/routes/utils.dart';
 import 'package:mvvm/view/homeScreen.dart';
 
 class LogScreen extends StatefulWidget {
@@ -10,15 +11,72 @@ class LogScreen extends StatefulWidget {
 }
 
 class _LogScreenState extends State<LogScreen> {
+  FocusNode emailFocusNode = FocusNode();
+  FocusNode passwordFocusNode = FocusNode();
+  ValueNotifier<bool> _obsecurePasssword = ValueNotifier<bool>(true);
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height * 1;
     return Scaffold(
-        body: InkWell(
-            onTap: () {
-              Navigator.pushNamed(context, RoutesNames.home);
-              // Navigator.push(context,
-              //     MaterialPageRoute(builder: (context) => HomeScreen()));
+        appBar: AppBar(
+          title: Text('Login'),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+            child: Column(children: [
+          TextFormField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            focusNode: emailFocusNode,
+            decoration: const InputDecoration(
+                labelText: 'Email',
+                hintText: 'Email',
+                prefixIcon: Icon(Icons.alternate_email)),
+            onFieldSubmitted: (value) {
+              // it bassecally for go threw email to paasword
+              // FocusScope.of(context).requestFocus(passwordFocusNode);
+              Utils.fieldFocus(context, emailFocusNode, passwordFocusNode);
             },
-            child: Center(child: Text('Clicked'))));
+          ),
+          ValueListenableBuilder(
+              valueListenable: _obsecurePasssword,
+              builder: (context, value, child) {
+                return TextFormField(
+                  controller: _passwordController,
+                  obscureText: _obsecurePasssword.value,
+                  focusNode: passwordFocusNode,
+                  obscuringCharacter: '*',
+                  decoration: InputDecoration(
+                      hintText: 'Password',
+                      labelText: 'Password',
+                      prefixIcon: const Icon(Icons.lock_open_rounded),
+                      suffixIcon: InkWell(
+                          onTap: () {
+                            _obsecurePasssword.value =
+                                !_obsecurePasssword.value;
+                          },
+                          child: Icon(_obsecurePasssword.value
+                              ? Icons.visibility_off_outlined
+                              : Icons.visibility))),
+                );
+              }),
+          SizedBox(
+            height: height * .1,
+          )
+        ])));
+    //       body: InkWell(
+    //           onTap: () {
+    //             Utils.flushBarErrorMessage('No internate connction', context);
+
+    //             // Utils.toastMessage('clicked me');
+    //             // Utils.snackBar('yess', context);
+    //             // Navigator.pushNamed(context, RoutesNames.home);
+    //             // Navigator.push(context,
+    //             //     MaterialPageRoute(builder: (context) => HomeScreen()));
+    //           },
+    //           child: Center(child: Text('Clicked')))
   }
 }
